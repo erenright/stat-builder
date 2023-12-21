500f4915229d46f0a5559e5f0cf30228cf28b16e
Eric Enright <erenright@protonmail.ch>
2023-12-20 22:20:23 -0500

    feat: Point Buy

--- stat-builder.js
+++ stat-builder.js
@@ -1,3 +1,8 @@
+
+//
+// Cards
+//
+
 var cards = [];
 
 function updateRemainingCards()
@@ -54,10 +59,129 @@ function drawCard()
     let cols = "<td>Result:</td>";    
     cols += "<td>" + first + " + " + second + " = " + (first + second) + "</td>";
     row.append(cols);
-    $("#cards").append(row);}
+    $("#cards").append(row);
+}
+
+//
+// Point Buy
+//
+
+var points = 0;
+var pointBuyStats = [];
+
+function pointBuy(index)
+{
+    // Input validation
+    if (index >= pointBuyStats.length || index < 0)
+    {
+        return;
+    }
+
+    // Enough money?
+    var cost = 0;
+    var id = "#pb" + index;
+    var want = parseInt($(id).html()) + 1;
+    switch (want)
+    {
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+            cost = 1;
+            break;
+
+        case 14:
+        case 15:
+            cost = 2;
+            break;
+
+        default:
+            alert("Sorry, this is not an option.");
+            return;
+    }
+
+    if (cost > points)
+    {
+        alert("You don't have enough points.");
+        return;
+    }
+    
+    // Purchase
+    points -= cost;
+    pointBuyStats[index] += 1;
+
+    // Update UI
+    updatePoints();
+}
+
+function pointSell(index)
+{
+    // Input validation
+    if (index >= pointBuyStats.length || index < 0)
+    {
+        return;
+    }
+
+    // Determine cost
+    var cost = 0;
+    var id = "#pb" + index;
+    var want = parseInt($(id).html());
+    switch (want)
+    {
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+            cost = 1;
+            break;
+
+        case 14:
+        case 15:
+            cost = 2;
+            break;
+
+        default:
+            alert("Sorry, this is not an option.");
+            return;
+    }
+
+    // Purchase
+    points += cost;
+    pointBuyStats[index] -= 1;
+
+    // Update UI
+    updatePoints();
+}
+
+function resetPoints()
+{
+    points = 27;
+    pointBuyStats = [ 8, 8, 8, 8, 8, 8 ];
+}
+
+function updatePoints()
+{
+    for (let i = 0; i < pointBuyStats.length; ++i)
+    {
+        var id = "#pb" + i;
+        $(id).html(pointBuyStats[i]);
+    }
+
+    $("#pb").html(points);
+}
+
+//
+// Common
+//
 
 $(document).ready(function ()
 {
     resetCards();
     updateRemainingCards();
+
+    resetPoints();
+    updatePoints();
 });
+
